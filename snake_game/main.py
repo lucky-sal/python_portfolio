0f99d496c533bf96b2445b93d8b0dfc4569a23ab
Sal <salbarnaojr@gmail.com>
2024-02-01 10:11:54 -0800

    Adding new training projects

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from score import Score
 import time
 
 screen = Screen()
@@ -9,6 +11,9 @@ screen.title('My Snake Game')
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Score()
+
 screen.listen()
 screen.onkey(snake.up, key='w')
 screen.onkey(snake.down, key='s')
@@ -18,7 +23,24 @@ screen.onkey(snake.right, key='d')
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(.1)
+    time.sleep(0.1)
     snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        score.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 7:
+            game_is_on = False
+            score.game_over()
 
 screen.exitonclick()
