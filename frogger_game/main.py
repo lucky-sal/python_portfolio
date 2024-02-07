c38a32f716e1116e5051b8fe146b7533c3cb4af2
Sal <salbarnaojr@gmail.com>
2024-02-07 11:10:35 -0800

    frogger game completed

--- frogger_game/main.py
+++ frogger_game/main.py
@@ -7,8 +7,33 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+screen.title('My Frogger Game')
+
+player = Player()
+scoreboard = Scoreboard()
+car = CarManager()
+
+screen.onkey(player.go_up, key='w')
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car.create_car()
+    car.car_move()
+
+    if player.ycor() > 280:
+        # increase level by getting to end
+        scoreboard.level_up()
+        player.level_up()
+        car.level_up()
+
+    # player collision with car
+    for x in car.all_cars:
+        if x.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
--- frogger_game/scoreboard.py
+++ frogger_game/scoreboard.py
@@ -1,5 +1,27 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+ALIGNMENT = 'center'
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.goto(-200, 250)
+        self.write(f'Level: {self.level}', align=ALIGNMENT, font=FONT)
+
+    def level_up(self):
+        self.level += 1
+        self.clear()
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.color('Red')
+        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
